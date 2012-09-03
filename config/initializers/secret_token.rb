9d7692d0efcbddfa4bb26012277b30cdccd09511
Kvanh Varma <kvanhvarma@Kvanhs-MacBook-Pro.local>
2012-09-03 12:53:48 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Girgit::Application.config.secret_token = 'c64c7e4d78a9b32a88e5b529e9206f72a03736d816a4768c92b9d3b238711e958980003a2b727cdf610c2c17820bc1a32428c27b787093c42cc858b811eba0a3'
